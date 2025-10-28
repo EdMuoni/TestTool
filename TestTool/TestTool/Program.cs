0cdf712223ce899ac6b92c7f3dcf5e78337d62f2
Edgar <edgar.muoni@gmail.com>
2025-10-28 11:37:55 +0200

    Creating TestTool

---
+++ TestTool/TestTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ToolsQA
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.demoqa.com";
+        }
+    }
+    
+}
